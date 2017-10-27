4c0818f5793ba1f445ac3e640853853e3e7b7a2b
Gabriel Parelli Francischini <gpfrancischini@gmail.com>
2017-10-27 16:49:25 -0200

    Save entire state into localStorage

--- src/index.js
+++ src/index.js
@@ -7,10 +7,25 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 
+// 
+let persistedState = {};
+try {
+    persistedState = localStorage.getItem("state") ? JSON.parse(localStorage.getItem("state")) : null;
+} 
+catch (e) {
+    console.log("The persisted stated failed to load: " + e);
+}
+
 const store = createStore(
     reducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+//where should I subscribe for this
+store.subscribe(() => {
+    localStorage.setItem("state", JSON.stringify(store.getState()));
+})
 
 ReactDOM.render(
     <Provider store={store}>
